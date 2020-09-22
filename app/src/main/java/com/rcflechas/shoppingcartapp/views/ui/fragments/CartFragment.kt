4743ad8c5075114ecd2f6f111a2beaef3cea444e
Renzon Caceres <renzon.caceres@wiedii.co>
2020-09-21 22:58:01 -0500

    Se creo la vista de carrito.

--- app/src/main/java/com/rcflechas/shoppingcartapp/views/ui/fragments/CartFragment.kt
+++ app/src/main/java/com/rcflechas/shoppingcartapp/views/ui/fragments/CartFragment.kt
@@ -7,21 +7,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.rcflechas.shoppingcartapp.R
 import com.rcflechas.shoppingcartapp.utilities.UIState
 import com.rcflechas.shoppingcartapp.viewmodels.MovieViewModel
 import com.rcflechas.shoppingcartapp.views.adapters.MovieAdapter
 import com.rcflechas.shoppingcartapp.views.binds.MovieBind
+import com.rcflechas.shoppingcartapp.views.widget.SwipeToDeleteCallback
 import kotlinx.android.synthetic.main.empty_view.*
-import kotlinx.android.synthetic.main.fragment_movie.*
+import kotlinx.android.synthetic.main.fragment_cart.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class MovieFragment : Fragment() {
+class CartFragment : Fragment() {
 
     private val movieViewModel: MovieViewModel by viewModel()
     private lateinit var  movieAdapter: MovieAdapter
@@ -32,16 +35,16 @@ class MovieFragment : Fragment() {
         setupHandler()
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movie, container, false)
+        return inflater.inflate(R.layout.fragment_cart, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initUI()
         setupToolbar()
-        //movieViewModel.getAllRemote()
+        movieViewModel.getAllRemote()
     }
 
     override fun onResume() {
@@ -63,20 +66,43 @@ class MovieFragment : Fragment() {
             setHasFixedSize(true)
             adapter = movieAdapter
         }
+
+        val swipeHandler = object : SwipeToDeleteCallback(requireActivity()) {
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+
+                val position = viewHolder.adapterPosition
+                val deletedItem = movieAdapter.getItem(position)
+                val data = movieAdapter.getData()
+                data.removeAt(position)
+                movieAdapter.animateTo(data)
+
+                // showing snack bar with Undo option
+                val snackbar = Snackbar
+                    .make(requireView(), "Item removed from list!", Snackbar.LENGTH_LONG)
+                snackbar.setAction("UNDO") {
+                    if (position == 0) {
+                        val layoutManager = moviesRecyclerView.layoutManager as LinearLayoutManager?
+                        layoutManager?.scrollToPosition(0)
+                    }
+                }
+                snackbar.setActionTextColor(Color.YELLOW)
+                snackbar.show()
+            }
+        }
+        val itemTouchHelper = ItemTouchHelper(swipeHandler)
+        itemTouchHelper.attachToRecyclerView(moviesRecyclerView)
     }
 
     private fun setupToolbar() {
-        toolbar.setTitle(R.string.app_name)
-        toolbar.inflateMenu(R.menu.menu)
 
-        toolbar.setOnMenuItemClickListener {
-            when (it.itemId) {
-                R.id.menu_main_cart -> {
-                    findNavController().navigate(R.id.action_movieFragment_to_cartFragment)
-                }
-            }
-            true
+        toolbar.navigationIcon = view?.context?.let { ContextCompat.getDrawable(it, R.drawable.ic_back_24) }
+        toolbar.setTitle(R.string.title_cart)
+        toolbar.setTitleTextColor(Color.WHITE)
+        toolbar.setNavigationOnClickListener {
+            requireActivity().onBackPressed()
         }
+
     }
 
     private fun setupHandler() {
@@ -89,12 +115,12 @@ class MovieFragment : Fragment() {
 
                     is UIState.Loading -> {
 
-                        Log.i(TAG, "--- Loading...")
+                        Log.i(MovieFragment.TAG, "--- Loading...")
                     }
                     is UIState.Success<*> -> {
 
                         val data = status.data as MutableList<MovieBind>
-                        Log.i(TAG, "--- Success...")
+                        Log.i(MovieFragment.TAG, "--- Success...")
                         if (data.count() != 0) {
 
                             includeEmptyView.visibility = View.GONE
@@ -109,7 +135,7 @@ class MovieFragment : Fragment() {
 
                         includeEmptyView.visibility = View.VISIBLE
                         loadingTextView.text = getString(R.string.message_connection_error)
-                        Log.i(TAG, "--- ${status.message}")
+                        Log.i(MovieFragment.TAG, "--- ${status.message}")
                     }
                 }
             }
@@ -117,8 +143,8 @@ class MovieFragment : Fragment() {
     }
 
     companion object {
-        const val TAG = "MovieFragment"
+        const val TAG = "CartFragment"
 
-        fun newInstance(bundle: Bundle? = null): MovieFragment = MovieFragment().apply { arguments = bundle }
+        fun newInstance(bundle: Bundle? = null): CartFragment = CartFragment().apply { arguments = bundle }
     }
 }
